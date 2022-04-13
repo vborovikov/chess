a7c2074672f82d6e2b403af0ca879480a530bbaa
Vladislav Borovikov <vborovikov@live.ru>
2022-04-13 20:58:34 +0500

    Use Selector

--- Chess/Chess.App/Controls/BoardControl.cs
+++ Chess/Chess.App/Controls/BoardControl.cs
@@ -1,13 +1,11 @@
 ﻿namespace Chess.App.Controls;
 
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 
 [TemplatePart(Name = TemplateParts.SquareGrid, Type = typeof(Grid))]
-public class BoardControl : ItemsControl
+public class BoardControl : Selector
 {
     private static class TemplateParts
     {
